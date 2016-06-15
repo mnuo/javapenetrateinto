acf837cde3115bc3e1c98e689f5fb3c779d1640b
mnuo <yu11115@163.com>
2016-06-16 00:04:03 +0800

    add runtime test1

---
+++ src/com/mnuocom/javapenetrateinto/runtime/RuntimeDemo1.java
@@ -0,0 +1,26 @@
+/**
+ * RuntimeDemo1.java created at Jun 16, 2016 12:03:38 AM
+ */
+package com.mnuocom.javapenetrateinto.runtime;
+
+/**
+ * @author saxon
+ */
+public class RuntimeDemo1 {
+
+	/**
+	 * 
+	 */
+	public RuntimeDemo1() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
