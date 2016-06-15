66785a241643ebf4417c2176fdb20010010d71b9
mnuo <yu11115@163.com>
2016-06-16 00:03:15 +0800

    add runtime class

---
+++ src/com/mnuocom/javapenetrateinto/runtime/RuntimeDemo.java
@@ -0,0 +1,26 @@
+/**
+ * RuntimeDemo.java created at Jun 16, 2016 12:02:55 AM
+ */
+package com.mnuocom.javapenetrateinto.runtime;
+
+/**
+ * @author saxon
+ */
+public class RuntimeDemo {
+
+	/**
+	 * 
+	 */
+	public RuntimeDemo() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
